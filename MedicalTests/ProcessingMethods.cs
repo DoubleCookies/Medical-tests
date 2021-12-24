e2450e9cc4a36f75c0cdc9749acf0bbcbc965667
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2021-12-24 12:37:48 +0300

    Splitting methods for format 1 + 2

--- MedicalTests/ProcessingMethods.cs
+++ MedicalTests/ProcessingMethods.cs
@@ -4,60 +4,81 @@ using System.Linq;
 
 namespace MedicalTests
 {
+    //TODO: можно отрефакторить сами методы обработки форматов, разбив на подблоки (вопрос, ответ)
     static class ProcessingMethods
     {
         private static readonly string[] answerProcPositive = new string[] { "shift", "~%100%", "~%50%", "~%33.333%", "~%25%", "~%20%", "~%16.666%", "~%14.289%", "~%12.5%", "~%11.111%", "~%10%", "~%9.091%", "~%8.333%" };
         private static readonly string[] answerProcNegative = new string[] { "shift", "~%-100%", "~%-50%", "~%-33.333%", "~%-25%", "~%-20%", "~%-16.666%", "~%-14.289%", "~%-12.5%", "~%-11.111%", "~%-10%", "~%-9.091%", "~%-8.333%" };
 
+        private static string resultText;
+
         public static string ProcessFormat1(string text, int startNum)
         {
-            string res = "";
+            resultText = "";
             string[] tasks = text.Split(new string[] { "***\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < tasks.Length; i++)
             {
                 string taskRes = $"::Q-{startNum + i}::";
                 string[] taskStrings = tasks[i].Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
 
-                //вопрос
+                int answersCount = 0;
+
                 string question = taskStrings[0].Trim();
-                int braceLeft = question.LastIndexOf('(');
-                int braceRight = question.LastIndexOf(')');
-                int tabPosition = question.IndexOf('\t');
-                taskRes += question.Substring(tabPosition + 1, braceLeft - tabPosition - 2).TrimEnd() + " {\r\n";
-                int answersCount = int.Parse(question.Substring(braceLeft + 1, braceRight - braceLeft - 1));
+                string formattedQuestion = ProcessQuestionFormat1(question, ref answersCount);
+                taskRes += formattedQuestion;
 
-                //номера ответов
                 string answers = taskStrings[taskStrings.Length - 1].Trim();
-                answers = answers.Replace("Ответ", "").Trim().Replace("ответ", "").Trim().Replace("(", "").Replace(")", "").Trim();
-                string[] answersNumbers = answers.Split(',');
-                int[] answersNumbersArray = Array.ConvertAll(answersNumbers, int.Parse);
+                int[] answersNumbersArray = GetAnswerNumbers(answers);
 
-                //сами ответы
-                for (int j = 1; j < taskStrings.Length - 1; j++)
-                {
-                    string answerString = taskStrings[j].Substring(j.ToString().Length + 1).Trim();
-                    if (answersCount == 1)
-                    {
-                        if (answersNumbersArray.Contains(j))
-                            taskRes += "=" + answerString + "\r\n";
-                        else
-                            taskRes += "~" + answerString + "\r\n";
-                    }
-                    else
-                    {
-                        int positive = answersNumbersArray.Length;
-                        int negative = taskStrings.Length - 2 - positive;
-                        if (answersNumbersArray.Contains(j))
-                            taskRes += answerProcPositive[positive] + answerString + "\r\n";
-                        else
-                            taskRes += answerProcNegative[negative] + answerString + "\r\n";
-                    }
-                }
+                string answersText = ProcessAnswers(taskStrings, answersNumbersArray);
+                taskRes += answersText;
 
                 taskRes += "}\r\n\r\n";
-                res += taskRes;
+                resultText += taskRes;
             }
-            return res;
+            return resultText;
+        }
+
+        private static string ProcessQuestionFormat1(string question, ref int answersCount) {
+            int braceLeft = question.LastIndexOf('(');
+            int braceRight = question.LastIndexOf(')');
+            int tabPosition = question.IndexOf('\t');
+            answersCount = int.Parse(question.Substring(braceLeft + 1, braceRight - braceLeft - 1));
+            string formattedQuestion = question.Substring(tabPosition + 1, braceLeft - tabPosition - 2).TrimEnd() + " {\r\n";
+            return formattedQuestion;
+        }
+
+        private static int[] GetAnswerNumbers(string answers) {
+            answers = answers.Replace("Ответ", "").Trim().Replace("ответ", "").Trim().Replace("(", "").Replace(")", "").Trim();
+            string[] answersNumbers = answers.Split(',');
+            int[] answersNumbersArray = Array.ConvertAll(answersNumbers, int.Parse);
+            return answersNumbersArray;
+        }
+
+        private static string ProcessAnswers(string[] taskStrings, int[] answerNumbers) {
+            string answersText = "";
+            //сами ответы
+            for (int j = 1; j < taskStrings.Length - 1; j++)
+            {
+                string answerString = taskStrings[j].Substring(j.ToString().Length + 1).Trim();
+                if (answerNumbers.Length == 1)
+                {
+                    if (answerNumbers.Contains(j))
+                        answersText += "=" + answerString + "\r\n";
+                    else
+                        answersText += "~" + answerString + "\r\n";
+                }
+                else
+                {
+                    int positive = answerNumbers.Length;
+                    int negative = taskStrings.Length - 2 - positive;
+                    if (answerNumbers.Contains(j))
+                        answersText += answerProcPositive[positive] + answerString + "\r\n";
+                    else
+                        answersText += answerProcNegative[negative] + answerString + "\r\n";
+                }
+            }
+            return answersText;
         }
 
         public static string ProcessFormat2(string text, int startNum)
@@ -69,60 +90,82 @@ namespace MedicalTests
                 string taskRes = $"::T-{startNum + i}::";
                 string[] taskStrings = tasks[i].Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
 
-                //вопрос
+                int answersCount = 0;
                 int questionString = 4;
-                string question = taskStrings[questionString].Trim();
-                int braceLeft = question.LastIndexOf('(');
-                int braceRight = question.LastIndexOf(')');
-                while (braceLeft == -1 && braceRight == -1)
-                {
-                    questionString++;
-                    question += "\r\n" + taskStrings[questionString].Trim();
-                    braceLeft = question.LastIndexOf('(');
-                    braceRight = question.LastIndexOf(')');
-                }
-                int tabPosition = question.IndexOf('\t');
-                taskRes += question.Substring(tabPosition + 1, braceLeft - tabPosition - 2).TrimEnd() + " {\r\n";
-                int answersCount = int.Parse(question.Substring(braceLeft + 1, braceRight - braceLeft - 1));
 
-                List<int> parts = new List<int>();
-                for (int j = 1; j < taskStrings.Length; j++)
-                {
-                    if (taskStrings[j].Trim().StartsWith("+"))
-                        parts.Add(j);
-                }
+                string formattedQuestion = ProcessQuestionsFormat2(taskStrings, ref answersCount);
+                taskRes += formattedQuestion;
 
-                int[] answersNumbersArray = parts.ToArray();
+                int[] answersNumbersArray = GetAnswersFormat2(taskStrings);
 
-                //сами ответы
-                for (int j = questionString + 1; j < taskStrings.Length; j++)
-                {
-                    string answerString = taskStrings[j].Trim();
-                    if (taskStrings[j].Trim().StartsWith("+"))
-                        answerString = taskStrings[j].Trim().Substring(1, answerString.Length - 1);
-
-                    if (answersCount == 1)
-                    {
-                        if (answersNumbersArray.Contains(j))
-                            taskRes += "=" + answerString + "\r\n";
-                        else
-                            taskRes += "~" + answerString + "\r\n";
-                    }
-                    else
-                    {
-                        int positive = answersNumbersArray.Length;
-                        int negative = taskStrings.Length - 1 - questionString - positive;
-                        if (answersNumbersArray.Contains(j))
-                            taskRes += answerProcPositive[positive] + answerString + "\r\n";
-                        else
-                            taskRes += answerProcNegative[negative] + answerString + "\r\n";
-                    }
-                }
+                taskRes += ProcessAnswersFormat2(questionString, taskStrings, answersNumbersArray);
 
                 taskRes += "}\r\n\r\n";
                 res += taskRes;
             }
             return res;
+        }
+
+        private static string ProcessQuestionsFormat2(string[] taskStrings, ref int answersCount) {
+            string resultText = "";
+            //вопрос
+            int questionString = 4;
+            string question = taskStrings[questionString].Trim();
+            int braceLeft = question.LastIndexOf('(');
+            int braceRight = question.LastIndexOf(')');
+            while (braceLeft == -1 && braceRight == -1)
+            {
+                questionString++;
+                question += "\r\n" + taskStrings[questionString].Trim();
+                braceLeft = question.LastIndexOf('(');
+                braceRight = question.LastIndexOf(')');
+            }
+            int tabPosition = question.IndexOf('\t');
+            resultText += question.Substring(tabPosition + 1, braceLeft - tabPosition - 2).TrimEnd() + " {\r\n";
+            answersCount = int.Parse(question.Substring(braceLeft + 1, braceRight - braceLeft - 1));
+            return resultText;
+        }
+
+        private static int[] GetAnswersFormat2(string[] taskStrings) {
+            List<int> parts = new List<int>();
+            for (int j = 1; j < taskStrings.Length; j++)
+            {
+                if (taskStrings[j].Trim().StartsWith("+"))
+                    parts.Add(j);
+            }
+
+            int[] answersNumbersArray = parts.ToArray();
+            return answersNumbersArray;
+        }
+
+        private static string ProcessAnswersFormat2(int questionString, string[] taskStrings, int[] answersNumbersArray) {
+            string resultText = "";
+
+            for (int j = questionString + 1; j < taskStrings.Length; j++)
+            {
+                string answerString = taskStrings[j].Trim();
+                if (taskStrings[j].Trim().StartsWith("+"))
+                    answerString = taskStrings[j].Trim().Substring(1, answerString.Length - 1);
+
+                if (answersNumbersArray.Length == 1)
+                {
+                    if (answersNumbersArray.Contains(j))
+                        resultText += "=" + answerString + "\r\n";
+                    else
+                        resultText += "~" + answerString + "\r\n";
+                }
+                else
+                {
+                    int positive = answersNumbersArray.Length;
+                    int negative = taskStrings.Length - 1 - questionString - positive;
+                    if (answersNumbersArray.Contains(j))
+                        resultText += answerProcPositive[positive] + answerString + "\r\n";
+                    else
+                        resultText += answerProcNegative[negative] + answerString + "\r\n";
+                }
+            }
+
+            return resultText;
         }
 
 
